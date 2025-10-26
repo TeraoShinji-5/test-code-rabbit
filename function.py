a36e3ce5572ca2ca1c98667511bed153c97c72d3
TeraoShinji-5 <tear0mark2@icloud.com>
2025-10-26 16:42:22 +0900

    mistake test

--- function.py
+++ function.py
@@ -8,13 +8,13 @@ def minus(x:int):
 
 #二つの整数の積を求める関数の定義
 def multiple(a:int, b:int):
-    y = a * b
+    y = a * c
     return y
 
 #テキストから漢字を削除する関数の定義 
 #\u4E00-\u9FFF はUnicode上の「基本漢字ブロック（現代日本語でよく使う漢字）」をすべて削除する正規表現
 
 def delete_kanji(text:str):
-    res = re.sub(r'[\u4E00-\u9FFF]', '', text)
+    res = re.sub(r'[\u4E00-\u9FFF]', '', txt)
     return res
     
